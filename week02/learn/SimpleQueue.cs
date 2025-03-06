0437ff9da760c947e591c154fb60c317459c2bbb
galiciaabraham <galicia.abraham@hotmail.com>
2025-03-05 19:38:46 -0700

    Debugged SimpleQueue Activity Week2

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -10,7 +10,7 @@
         queue.Enqueue(100);
         var value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found:
+        // Defect(s) Found: The Dequeue function was removing index 1 instead of 0, The Enqueue function was inserting instead of adding. 
 
         Console.WriteLine("------------");
 
@@ -54,7 +54,7 @@
     /// </summary>
     /// <param name="value">Integer value to add to the queue</param>
     private void Enqueue(int value) {
-        _queue.Insert(0, value);
+        _queue.Add(value);
     }
 
     /// <summary>
@@ -64,10 +64,13 @@
     /// <returns>First integer in the queue</returns>
     private int Dequeue() {
         if (_queue.Count <= 0)
+        {
             throw new IndexOutOfRangeException();
-
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        } 
+        
+        var value = _queue[0];
+        _queue.RemoveAt(0);
         return value;
+
     }
 }
