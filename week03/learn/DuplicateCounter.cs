ed92d87e5c645c8097c79466585182141d3dc940
galiciaabraham <galicia.abraham@hotmail.com>
2025-03-12 19:38:56 -0600

    Finished learning activity for Sets.

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -1,4 +1,6 @@
-﻿public class DuplicateCounter
+﻿using System.Data;
+
+public class DuplicateCounter
 {
     //Count how many duplicates are in a collection of data.
 
@@ -24,7 +26,8 @@
 
     private static int CountDuplicates(int[] data)
     {
-        // Add code here.
-        return 0;
+        var duplicatesSet = new HashSet<int>(data);
+
+        return duplicatesSet.Count;
     }
 }
